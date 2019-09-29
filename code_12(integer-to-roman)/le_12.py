6e64e0cb7ba3edc66f9e3eba535c2ef023fcbae3
suiuieliushang <2837644648@qq.com>
2019-09-29 20:58:06 +0800

    13题

--- code_12(integer-to-roman)/le_12.py
+++ code_12(integer-to-roman)/le_12.py
@@ -1,6 +1,19 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         str=''
-        dict={1000:'M',900:'CM',400:'CD',100:'C',90:'XC',
-        '50':'L',40:'XL',10:'X',9:'IX',5:'V',4:'IV',1:'I'}
-        
+        dict={1000:'M',900:'CM',500:'D',400:'CD',100:'C',90:'XC',
+        50:'L',40:'XL',10:'X',9:'IX',5:'V',4:'IV',1:'I'}
+        a=0
+        while num>=1:
+            last=num%10
+            if dict.__contains__(last*10**a):
+                str=dict[last*10**a]+str
+            elif last>5:
+                str=dict[5*10**a]+(last-5)*dict[10**a]+str
+            else:
+                str=last*dict[10**a]+str
+            a+=1
+            num=int(num/10)
+        return str
+s=Solution()
+print(s.intToRoman(301))
---
+++ code_13(roman-to-integer)/le_13.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        #罗马数字转换为整数
+        dict={'M':1000,'CM':900,'D':500,'CD':400,'C':100,'XC':90,
+        'L':50,'XL':40,'X':10,'IX':9,'V':5,'IV':4,'I':1}
+        integer=0   
+
+        i=0
+        while i<len(s):
+            if i<len(s)-1:#当不为最后一个字符时 
+                if dict.__contains__(s[i:i+2]):#先判断连续两个的
+                    integer+=dict[s[i:i+2]]
+                    i+=2
+                    continue
+            integer+=dict[s[i]]
+            i+=1
+        return integer
+
+print(Solution().romanToInt("LVIII"))
