5873c583ed41e8042389b7af8a9dd76287a3d767
suiuieliushang <2837644648@qq.com>
2019-09-29 21:29:40 +0800

    十四题

---
+++ code_14(longest-common-prefix)/le_14.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestCommonPrefix(self, strs: [str]) -> str:
+        if  not strs:
+            return ''
+        lc=strs[0]
+        for i in strs:
+            j=0
+            a=min(len(lc),len(i))
+            while j<a and lc[j]==i[j]:
+                j+=1
+            lc=lc[0:j]
+            if lc=='':
+                return ''
+        return lc
