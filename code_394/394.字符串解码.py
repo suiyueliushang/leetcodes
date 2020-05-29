735a706a6101a78bf9a6590debf037f6175c106f
suiyueliushang <2837644648@qq.com>
2020-05-29 12:18:33 +0800

    5/29

--- code_394/394.字符串解码.py
+++ code_394/394.字符串解码.py
@@ -5,35 +5,39 @@
 #
 
 # @lc code=start
+
+
 class Solution:
-	def decodeString(self, s: str) -> str:
-		if s==None or s=="":return ""
-		stack,left_bracket,number=[],[],[]
-		i=0
-		while i<s.__len__():
-			if s[i].isalpha():
-				stack.append(s[i])
-			elif s[i]=='[':
-				left_bracket.append(stack.__len__())
-				stack.append('[')
-			elif s[i].isdigit():
-				dig=[]
-				while(s[i].isdigit()):
-					dig.append(s[i])
-					i+=1	
-				number.append(int("".join(dig)))
-				i-=1
-			elif s[i]==']':
-				if left_bracket.__len__()==0 or number.__len__()==0:
-					return None
-				else:
-					j=left_bracket.pop()
-					stack[j:]=list(number.pop()*stack[j+1:])
-			else:
-				stack.append(s[i])
-			i+=1
-		return ''.join(stack)
+    def decodeString(self, s: str) -> str:
+        if s == None or s == "":
+            return ""
+        stack, left_bracket, number = [], [], []
+        i = 0
+        while i < s.__len__():
+            if s[i].isalpha():
+                stack.append(s[i])
+            elif s[i] == '[':
+                left_bracket.append(stack.__len__())
+                stack.append('[')
+            elif s[i].isdigit():
+                dig = []
+                while(s[i].isdigit()):
+                    dig.append(s[i])
+                    i += 1
+                number.append(int("".join(dig)))
+                i -= 1
+            elif s[i] == ']':
+                if left_bracket.__len__() == 0 or number.__len__() == 0:
+                    return None
+                else:
+                    j = left_bracket.pop()
+                    stack[j:] = list(number.pop()*stack[j+1:])
+            else:
+                stack.append(s[i])
+            i += 1
+        return ''.join(stack)
 # @lc code=end
 
-S=Solution()
+
+S = Solution()
 print(S.decodeString("30[leet]"))
